350139ce12b811fc60e891b986ff0f9f5b311884
tambroustail <chloe@nexm.io>
2024-08-21 16:03:01 +0700

    Update bounty_contest.rs

--- src/handlers/bounty_contest.rs
+++ src/handlers/bounty_contest.rs
@@ -1,28 +1,46 @@
 use actix_multipart::Multipart;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
+use deadpool_postgres::{Client, Pool, PoolError};
 use csv::ReaderBuilder;
 use futures_util::StreamExt as _;
 use serde::Serialize;
 use std::io::Cursor;
+use std::sync::Arc;
 // use futures::StreamExt;
 use serde::Deserialize;
 use std::io::{BufReader, BufWriter};
-use tokio_postgres::{Client, Error, NoTls}; 
+use tokio_postgres::{Error, NoTls}; 
 #[derive(Debug, Deserialize, Serialize)] 
 struct Record {
     Email: String, 
     Username: String,
     Points: i32,
+
+
 }
 
-#[get("/")]
+#[derive(Clone, Debug)]
+pub struct BountyDao {
+    db: Arc<Pool>,
+}
+
+
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/api")]
-async fn api(mut payload: Multipart) -> actix_web::Result<HttpResponse> {
+impl BountyDao{
+    pub fn new(db: Arc<Pool>) -> Self {
+        BountyDao { db: db.clone() }
+    }
+
+
+
+
+// Use default implementation for `error_response()` method
+
+
+    pub async fn api(&self, mut payload: Multipart) -> (actix_web::Result<HttpResponse>){
     // Iterate over the multipart stream
     let mut results: Vec<Record> = Vec::new();
     while let Some(item) = payload.next().await {
@@ -43,19 +61,16 @@ async fn api(mut payload: Multipart) -> actix_web::Result<HttpResponse> {
         }
     }
     // Connect to the database
-    let (client, connection) = tokio_postgres::connect(
-        "host=127.0.0.1 user=username dbname=postgres password=password",
-        NoTls,
-    )
-    .await
-    .unwrap();
+    let client: Client = self.db.get().await?;
+ 
+
+    
 
     // Spawn the connection to run in the background
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("Connection error: {}", e);
-        }
-    });
+  
+
+    // Spawn the connection to run in the background
+    
 
     for record in &results {
         // Check if the email exists in the RANKINGS table
@@ -94,24 +109,11 @@ async fn api(mut payload: Multipart) -> actix_web::Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(results))
 }
 
-#[get("/dashboard")]
-async fn dashboard(query: web::Query<PagingParams>) -> impl Responder {
+    pub async fn dashboard(&self,query: web::Query<PagingParams>) -> actix_web::Result<HttpResponse> {
     let page: i64 = query.page.unwrap_or(1).try_into().unwrap();
     let per_page: i64 = query.per_page.unwrap_or(10).try_into().unwrap();
+    let client: Client = self.db.get().await?;
 
-    let (client, connection) = tokio_postgres::connect(
-        "host=127.0.0.1 user=username dbname=postgres password=password",
-        NoTls,
-    )
-    .await
-    .unwrap();
-
-    // Spawn the connection to run in the background
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("Connection error: {}", e);
-        }
-    });
 
     // Query to fetch items and total count from the database
     let offset: i64 = (page - 1) * per_page;
@@ -137,9 +139,17 @@ async fn dashboard(query: web::Query<PagingParams>) -> impl Responder {
         })
         .collect();
 
-    let html = render_html(&items, page, per_page, total_items);
-    HttpResponse::Ok().content_type("text/html").body(html)
+    //let html = render_html(&items, page, per_page, total_items);
+   
+    Ok(HttpResponse::Ok().json(items))
 }
+
+}
+
+
+
+
+
 
 #[derive(Deserialize)]
 struct PagingParams {
@@ -199,10 +209,14 @@ fn render_html(items: &[Record], page: i64, per_page: i64, total_items: i64) -> 
     html
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(dashboard).service(api))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+
+
+
+pub fn route(conf: &mut web::ServiceConfig) {
+    conf.service(
+        web::scope("/bounty_contest")
+            .route("/", web::post().to(api))
+            .route("/", web::get().to(dashboard))
+            
+    );
 }
