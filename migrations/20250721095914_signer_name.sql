9a39357e8a08e4400bdcb46a1fa9f775098128f2
son.pham@nexm.io <son.pham@nexm.io>
2025-07-21 18:41:03 +0700

    migrate multisig-signers

--- migrations/20250721095914_signer_name.sql
+++ migrations/20250721095914_signer_name.sql
@@ -1,21 +1,3 @@
 -- Add migration script here
 ALTER TABLE multi_sig_signers ADD COLUMN signer_name VARCHAR(255);
 ALTER TABLE multi_sig_invites ADD COLUMN signer_name VARCHAR(255);
-
-UPDATE multi_sig_signers mss
-SET signer_name = ab.signer_name
-FROM (
-    SELECT signer_address, MAX(signer_name) AS signer_name
-    FROM address_books
-    GROUP BY signer_address
-) ab
-WHERE mss.signer_address = ab.signer_address;
-
-UPDATE multi_sig_invites msi
-SET signer_name = ab.signer_name
-FROM (
-    SELECT signer_address, MAX(signer_name) AS signer_name
-    FROM address_books
-    GROUP BY signer_address
-) ab
-WHERE msi.signer_address = ab.signer_address;
