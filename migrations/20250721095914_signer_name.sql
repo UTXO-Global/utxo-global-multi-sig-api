d43614c4b708c486d9806d3577222c658aee03b0
son.pham@nexm.io <son.pham@nexm.io>
2025-07-21 17:28:59 +0700

    add signer name to migrate

--- migrations/20250721095914_signer_name.sql
+++ migrations/20250721095914_signer_name.sql
@@ -1,3 +1,3 @@
 -- Add migration script here
-ALTER TABLE multi_sig_signers ADD COLUMN signer_name VARCHAR(25);
+ALTER TABLE multi_sig_signers ADD COLUMN signer_name VARCHAR(255);
 ALTER TABLE multi_sig_invites ADD COLUMN signer_name VARCHAR(255);
